40a7a9fba7edc958ac9bade9a06982e47254cde6
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2015-11-26 13:42:25 +0100

    Fixe enviroment variable set via .sh script

--- prepareCodeEnvironment.sh
+++ prepareCodeEnvironment.sh
@@ -3,7 +3,7 @@
 echo -e "\nSetting environment variables..."
 echo "\$SSP_CODE_HOME = $PWD"
 
-export SSP_CODE_HOME=$PWD
+setx.exe SSP_CODE_HOME $PWD
 
 echo -e "\nCopying git hooks..."
 cp tools/githook/* .git/hooks/
