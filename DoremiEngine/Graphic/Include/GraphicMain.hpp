8d71ada5bf102e4e769f20bae99fe9d7f69aa054
Konrad Öhman <konrad_o_92@hotmail.com>
2015-12-01 16:46:02 +0100

    Added initialization of DirX
    Initialization of DirX should be done, no windows creation though

--- DoremiEngine/Graphic/Include/GraphicMain.hpp
+++ DoremiEngine/Graphic/Include/GraphicMain.hpp
@@ -1,6 +1,14 @@
 #pragma once
 // Project specific
 
+
+// Third party
+#include <dxgi.h>
+#include <d3d11_1.h>
+//Not used?
+#include <d3dcommon.h>
+#include <d3dcompiler.h>
+
 // Standard libraries
 #include <vector>
 #include <string>
@@ -27,6 +35,15 @@ namespace DoremiEngine
         private:
             static GraphicMain* m_singleton;
             GraphicMain();
+            void InitializeDirectX();
+
+            //Direct X main stuff
+            ID3D11Device* m_device;
+            ID3D11DeviceContext* m_deviceContext;
+            IDXGISwapChain* m_swapChain;
+            ID3D11RenderTargetView* m_backBuffer;
+            ID3D11Texture2D* m_depthBuffer;
+            ID3D11DepthStencilView* m_depthView;
 
             std::vector<VertexShader*> m_vertexShaders;
             std::vector<PixelShader*> m_pixelShaders;
--- DoremiEngine/Graphic/Source/GraphicMain.cpp
+++ DoremiEngine/Graphic/Source/GraphicMain.cpp
@@ -26,7 +26,104 @@ namespace DoremiEngine
         GraphicMain::~GraphicMain()
         {
         }
-        
+
+        void GraphicMain::InitializeDirectX()
+        {
+            DXGI_SWAP_CHAIN_DESC scd;
+
+            // clear out the struct for use
+            ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+            // fill the swap chain description struct
+            scd.BufferCount = 1;                                    // one back buffer
+            scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+            scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+            scd.OutputWindow = GetActiveWindow();                                // the window to be used
+            scd.SampleDesc.Count = 4;                               // how many multisamples
+            scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+            HRESULT res = D3D11CreateDeviceAndSwapChain(NULL,
+                D3D_DRIVER_TYPE_HARDWARE,
+                NULL,
+                D3D11_CREATE_DEVICE_DEBUG,
+                NULL,
+                NULL,
+                D3D11_SDK_VERSION,
+                &scd,
+                &m_swapChain,
+                &m_device,
+                NULL,
+                &m_deviceContext);
+
+            ID3D11Texture2D* t_BackBuffer;
+            m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&t_BackBuffer);
+            m_device->CreateRenderTargetView(t_BackBuffer, NULL, &m_backBuffer);
+            t_BackBuffer->Release();
+
+            //Rasterizer info, here you can change from backface culling mm
+            //We might want this as a class or something to enable easy switching between rasterizers
+            D3D11_RASTERIZER_DESC rastDesc;
+            ZeroMemory(&rastDesc, sizeof(rastDesc));
+            rastDesc.FillMode = D3D11_FILL_SOLID;
+            rastDesc.CullMode = D3D11_CULL_NONE;
+            rastDesc.FrontCounterClockwise = false;
+            rastDesc.DepthBias = 0;
+            rastDesc.DepthBiasClamp = 0.0f;
+            rastDesc.SlopeScaledDepthBias = 0.0f;
+            rastDesc.DepthClipEnable = false;
+            rastDesc.ScissorEnable = false;
+            rastDesc.MultisampleEnable = true;
+            rastDesc.AntialiasedLineEnable = false;
+            ID3D11RasterizerState* tRastHandle;
+            res = m_device->CreateRasterizerState(&rastDesc, &tRastHandle);
+            m_deviceContext->RSSetState(tRastHandle);
+
+            //Depth buffer
+            //Might want this in a class for readability and easy changing between states
+            D3D11_TEXTURE2D_DESC dbdesc;
+            ZeroMemory(&dbdesc, sizeof(dbdesc));
+            dbdesc.Width = 800;
+            dbdesc.Height = 600;
+            dbdesc.MipLevels = 1;
+            dbdesc.ArraySize = 1;
+            dbdesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+            dbdesc.SampleDesc.Count = 4;
+            dbdesc.SampleDesc.Quality = 0;
+            dbdesc.Usage = D3D11_USAGE_DEFAULT;
+            dbdesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+            dbdesc.CPUAccessFlags = 0;
+            dbdesc.MiscFlags = 0;
+
+            res = m_device->CreateTexture2D(&dbdesc, NULL, &m_depthBuffer);
+
+            D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+            ZeroMemory(&descDSV, sizeof(descDSV));
+
+            descDSV.Format = dbdesc.Format;
+            descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+            descDSV.Texture2D.MipSlice = 0;
+
+
+            res = m_device->CreateDepthStencilView(m_depthBuffer, &descDSV, &m_depthView);
+            m_deviceContext->OMSetRenderTargets(1, &m_backBuffer, m_depthView);
+
+            //Viewport
+            //TODOKO change so width and height is modifiable and taken from some smarter place
+            D3D11_VIEWPORT viewport;
+            ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+            viewport.TopLeftX = 0;
+            viewport.TopLeftY = 0;
+            viewport.Width = 800;
+            viewport.Height = 600;
+            viewport.MinDepth = 0.0f;
+            viewport.MaxDepth = 1.0f;
+
+            m_deviceContext->RSSetViewports(1, &viewport);
+
+            //TODO add more different things like transparancy
+        }
+
         int GraphicMain::LoadShader(const ShaderType& p_type, const std::string& p_fileName)
         {
             int returnID;
