c438a6249eaae3064cdfb7d69587f16336c3b831
John Bergsten <putte_dt@hotmail.se>
2015-12-11 12:14:46 +0100

    Added some comments to rigidbody interface

--- DoremiEngine/Physics/Include/RigidBodyManager.hpp
+++ DoremiEngine/Physics/Include/RigidBodyManager.hpp
@@ -12,9 +12,13 @@ namespace DoremiEngine
             Adds a dynamic box body to the world. Returns the ID for the body.
             Very basic method and will probably be expanded several*/
             virtual int AddBoxBodyDynamic(XMFLOAT3 p_position, XMFLOAT4 p_orientation, XMFLOAT3 p_dims, int p_materialID) = 0;
+
+            /// Manipulators
             /**
             Adds a force to a body*/
             virtual void AddForceToBody(int p_bodyID, XMFLOAT3 p_force) = 0;
+
+            /// Getters
             /**
             Gets the position of a body*/
             virtual XMFLOAT3 GetBodyPosition(int p_bodyID) = 0;
