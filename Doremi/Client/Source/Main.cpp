694e69d1b01f0a8b8792ab6b8a372aad701b6720
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2015-12-15 16:13:47 +0100

    Cleaned up mailfile somewhat

--- Doremi/Client/Source/Main.cpp
+++ Doremi/Client/Source/Main.cpp
@@ -9,45 +9,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
-
-#include <thread> // std::this_thread::sleep_for
-#include <chrono> // std::chrono::seconds
 #else
 #error Platform not supported
 #endif
 #include <exception>
 #include <iostream>
-#include <Utility\MemoryManager\Include\MainMemoryManager.hpp>
-#include <Utility/DebugLog/Include/VirtualConsole.hpp>
+
 #ifdef _WIN32
-// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 int main(int argc, const char* argv[])
 #else
 #error Platform not supported
 #endif
 {
     FreeConsole();
-    Utility::DebugLog::VirtualConsole a;
-    Utility::DebugLog::VirtualConsole b;
-    a.Initialize();
-    b.Initialize("physics", true, true, Utility::DebugLog::ConsoleColorEnum::YELLOW);
-    int i = 0;
-    int j = 0;
-    while(true)
-    {
-        a.LogText(Utility::DebugLog::LogTag::NETWORK, Utility::DebugLog::LogLevel::INIT_PRINT, "Test %d", ++i);
-        b.LogText(Utility::DebugLog::LogTag::WATER, Utility::DebugLog::LogLevel::FATAL_ERROR, "T3st %d", ++j);
-
-
-        //      Utility::MemoryManager::MainMemoryManager::Startup();
-        //      auto& a = Utility::MemoryManager::MainMemoryManager::GetInstance();
-        //    Utility::MemoryManager::MainMemoryManager::Shutdown();
-        if(i == 10)
-        {
-            break;
-        }
-    }
-
     try
     {
         Doremi::GameMain gameMain = Doremi::GameMain();
@@ -55,7 +29,7 @@ int main(int argc, const char* argv[])
     }
     catch(const std::exception& e)
     {
-        // TODO more information
+        // TODORT more information
         std::cout << "Unknown exception: " << e.what() << std::endl;
         std::cin.get();
     }
