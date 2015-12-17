89dcadad814ef3eedcc0a201b458b51c39241001
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2015-12-17 16:46:29 +0100

    Better logfilename and some code cleanup

--- Utility/ConsoleApplication/Source/Main.cpp
+++ Utility/ConsoleApplication/Source/Main.cpp
@@ -18,6 +18,32 @@ inline wstring s2ws(const std::string& str)
     return converterX.from_bytes(str);
 }
 
+inline string getTwoDigit(const int& value)
+{
+    if(value < 10)
+    {
+        return string("0" + value);
+    }
+    return to_string(value);
+}
+
+inline string buildLogFileName(const string& p_consoleName)
+{
+    struct tm now;
+    time_t time_create = time(NULL);
+    localtime_s(&now, &time_create);
+
+    string fileName = string("logs/");
+    fileName.append(getTwoDigit(now.tm_year + 1900) + "-");
+    fileName.append(getTwoDigit(now.tm_mon + 1) + "-");
+    fileName.append(getTwoDigit(now.tm_mday) + "_");
+    fileName.append(getTwoDigit(now.tm_hour) + ".");
+    fileName.append(getTwoDigit(now.tm_min));
+    fileName.append("__" + p_consoleName);
+    fileName.append(".txt");
+    return fileName;
+}
+
 #define BUFSIZE 4096
 int main(int argc, char** argv)
 {
@@ -34,6 +60,7 @@ int main(int argc, char** argv)
 
     if(argc > 4)
     {
+        // TODORT Handle client and server logs
         consoleName = string(argv[1]);
         color = atoi(argv[2]);
         writeToConsole = atoi(argv[3]);
@@ -43,12 +70,18 @@ int main(int argc, char** argv)
         hStdin = GetStdHandle(STD_INPUT_HANDLE);
         if(hStdin == INVALID_HANDLE_VALUE)
         {
-            ExitProcess(1);
+#ifdef _DEBUG
+            system("pause");
+#endif
+            ExitProcess(1); // TODORT more info
         }
     }
     else
     {
-        return 1;
+#ifdef _DEBUG
+        system("pause");
+#endif
+        ExitProcess(1); // TODORT more info
     }
 
     if(writeToConsole)
@@ -57,7 +90,7 @@ int main(int argc, char** argv)
 
         if(out == INVALID_HANDLE_VALUE)
         {
-            return 1;
+            writeToConsole = false;
         }
         SetConsoleTitle(s2ws(consoleName).c_str());
         SetConsoleTextAttribute(out, color);
@@ -73,18 +106,24 @@ int main(int argc, char** argv)
         CreateDirectory(L"logs", NULL);
 
         // Build logfilename
-        struct tm now;
-        time_t time_create = time(NULL);
-        localtime_s(&now, &time_create);
-        consoleName = string("logs/" + std::to_string(now.tm_hour) + "." + std::to_string(now.tm_min) + "-" + consoleName);
-        consoleName.append(".txt");
+        const string& fileName = buildLogFileName(consoleName);
 
         // Create the file
-        fileHandle = CreateFile(s2ws(consoleName).c_str(), GENERIC_WRITE, FILE_SHARE_READ, 0, OPEN_ALWAYS, 0, 0);
+        fileHandle = CreateFile(s2ws(fileName).c_str(), GENERIC_WRITE, FILE_SHARE_READ, 0, OPEN_ALWAYS, 0, 0);
+        // TODO Add check to create another logfile if one already exists, because two clients may be runned at the same computer.
+
         if(fileHandle == INVALID_HANDLE_VALUE)
         {
-            return 1;
+            writeToFile = false;
         }
+    }
+
+    if(!writeToFile || !writeToConsole)
+    {
+#ifdef _DEBUG
+        system("pause");
+#endif
+        ExitProcess(1); // TODORT more info
     }
 
     while(true)
@@ -105,5 +144,5 @@ int main(int argc, char** argv)
             WriteFile(fileHandle, chBuf, dwRead, &dwRead, 0);
         }
     }
-    return 1;
+    return 0;
 }
