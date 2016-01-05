f147b5c5b6f691e183baed949589a36e68e736f2
John Bergsten <putte_dt@hotmail.se>
2016-01-05 16:38:23 +0100

    PhysicsModule: Added documentation

--- DoremiEngine/Physics/Include/PhysicsModule.hpp
+++ DoremiEngine/Physics/Include/PhysicsModule.hpp
@@ -25,11 +25,27 @@ namespace DoremiEngine
         class PhysicsModule : public DoremiEngine::Core::EngineModule
         {
             public:
+            /**
+            Does nothing so far TODOJB update as necessary
+            */
             virtual void Update(float p_dt) = 0;
 
+            /**
+            Gets the rigid body manager. This manager handles all rigid bodies.
+            It can be used to apply all kinds of forces, velocities and positions
+            to bodies. Its functionality is expanded as necessary
+            */
             virtual RigidBodyManager& GetRigidBodyManager() = 0;
+            /**
+            Gets the material manager. This manager handles all physical materials.
+            This is used to changed attributes of certain materials. It probably will
+            not be used all that much. Its functionality is exapnded as necessary
+            */
             virtual PhysicsMaterialManager& GetPhysicsMaterialManager() = 0;
-
+            /**
+            Gets a vector if all collision pairs. A collision pair consists of ids
+            of two bodies which have collided in the last simulation. This list is
+            automatically cleared before each new simulation*/
             virtual std::vector<CollisionPair> GetCollisionPairs() = 0;
 
             // Test function
