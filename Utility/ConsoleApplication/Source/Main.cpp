0b0036974470f9a13bd45fa7b8044ea6b4dcdd9f
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-01-07 17:20:43 +0100

    Fixed customconsoles to not close down directly...

--- Utility/ConsoleApplication/Source/Main.cpp
+++ Utility/ConsoleApplication/Source/Main.cpp
@@ -118,7 +118,7 @@ int main(int argc, char** argv)
         }
     }
 
-    if(!writeToFile || !writeToConsole)
+    if(!writeToFile && !writeToConsole)
     {
 #ifdef _DEBUG
         system("pause");
