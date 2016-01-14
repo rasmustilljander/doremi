9ab454c365a39ffa69bba119dc6882b34f74edc6
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-01-14 11:07:37 +0100

    Added some in-depth tests for the fixedsizedmempool

--- UnitTest/Source/Memory/FixedSizePoolAllocatorTest.cpp
+++ UnitTest/Source/Memory/FixedSizePoolAllocatorTest.cpp
@@ -24,10 +24,121 @@ TEST_F(FixedSizePoolAllocatorTest, initAlignment4)
     ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 4100);
 }
 
-/*
-TEST_F(FixedSizePoolAllocatorTest, AllocationFaultyOddAlignment)
+TEST_F(FixedSizePoolAllocatorTest, basicAllocation)
 {
-    ASSERT_THROW(m_poolAllocator->Initialize(64, 3), std::runtime_error);
- //   ASSERT_THROW(m_stackAllocator->Allocate<TestStruct64>(3), std::runtime_error);
+    m_poolAllocator->Initialize(64, 0);
+    TestStruct64* test = m_poolAllocator->Allocate();
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 4032);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 64);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 4096);
+    ASSERT_TRUE(test != nullptr);
 }
-*/
+
+TEST_F(FixedSizePoolAllocatorTest, basicAllocationLocation)
+{
+    m_poolAllocator->Initialize(64, 0);
+    TestStruct64* first = m_poolAllocator->Allocate();
+    TestStruct64* second = m_poolAllocator->Allocate();
+
+    ASSERT_NE(first, second);
+    ASSERT_TRUE(first < second);
+}
+
+template <typename T> void loopAllocate(FixedSizePoolAllocator<T>* p_allocator, const size_t& p_count)
+{
+    T* prev = p_allocator->Allocate();
+
+    for(size_t i = 0; i < p_count - 1; ++i)
+    {
+        T* current = p_allocator->Allocate();
+        ASSERT_TRUE(prev < current);
+        ASSERT_TRUE(current != nullptr);
+    }
+}
+
+TEST_F(FixedSizePoolAllocatorTest, halfAllocation)
+{
+    m_poolAllocator->Initialize(64, 0);
+    loopAllocate<TestStruct64>(m_poolAllocator, 32);
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 2048);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 2048);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 4096);
+}
+
+TEST_F(FixedSizePoolAllocatorTest, fullAllocation)
+{
+    m_poolAllocator->Initialize(64, 0);
+    loopAllocate<TestStruct64>(m_poolAllocator, 64);
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 0);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 4096);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 4096);
+}
+
+TEST_F(FixedSizePoolAllocatorTest, overAllocation)
+{
+    m_poolAllocator->Initialize(64, 0);
+    ASSERT_THROW(loopAllocate<TestStruct64>(m_poolAllocator, 65), std::runtime_error);
+}
+
+TEST_F(FixedSizePoolAllocatorTest, largeAllocation)
+{
+    m_poolAllocator->Initialize(1562500, 0);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 100000000);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 0);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 100000000);
+
+    loopAllocate<TestStruct64>(m_poolAllocator, 1562500);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 0);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 100000000);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 100000000);
+}
+
+TEST_F(FixedSizePoolAllocatorTest, basicFree)
+{
+    m_poolAllocator->Initialize(64, 0);
+
+    TestStruct64* pointer = m_poolAllocator->Allocate();
+    loopAllocate<TestStruct64>(m_poolAllocator, 63);
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 0);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 4096);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 4096);
+
+    m_poolAllocator->Free(pointer);
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 64);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 4032);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 4096);
+}
+
+TEST_F(FixedSizePoolAllocatorTest, freeLogicOnLargeSet)
+{
+    m_poolAllocator->Initialize(1562500, 0);
+    TestStruct64* first = m_poolAllocator->Allocate();
+    loopAllocate<TestStruct64>(m_poolAllocator, 781250 - 2); // Allocate about half
+    TestStruct64* mid = m_poolAllocator->Allocate();
+    loopAllocate<TestStruct64>(m_poolAllocator, 781250 - 1); // Allocate the resthalf
+    TestStruct64* end = m_poolAllocator->Allocate();
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 0);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 100000000);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 100000000);
+
+    m_poolAllocator->Free(mid);
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 64);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 99999936);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 100000000);
+
+    TestStruct64* newest = m_poolAllocator->Allocate();
+    ASSERT_TRUE(first < newest);
+    ASSERT_TRUE(newest < end);
+    ASSERT_EQ(newest, mid);
+
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().free, 0);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().occupied, 100000000);
+    ASSERT_EQ(m_poolAllocator->GetMemorySpecification().total, 100000000);
+}
