5258452361869b18ac9149cfef600e02fd6a5573
Lucas Holmqvist <lucas.holmqvist@gmail.com>
2016-01-20 08:42:29 +0100

    Added support for moving platform with sound
    On the server. Is not complete and has not been tested

--- Doremi/Core/Source/Manager/FrequencyAffectedObjectManager.cpp
+++ Doremi/Core/Source/Manager/FrequencyAffectedObjectManager.cpp
@@ -5,6 +5,7 @@
 #include <EntityComponent/Components/TransformComponent.hpp>
 #include <EntityComponent/Components/RigidBodyComponent.hpp>
 #include <EntityComponent/Components/TransformComponent.hpp>
+#include <EntityComponent/Components/PlatformPatrolComponent.hpp>
 #include <DoremiEngine/Physics/Include/RigidBodyManager.hpp>
 #include <Doremi/Core/Include/PlayerHandler.hpp>
 #include <Doremi/Core/Include/Helper/ProximityChecker.hpp>
@@ -50,6 +51,38 @@ namespace Doremi
                         if(ProximityChecker::GetInstance().CheckProximityToEntity(playerID->second->m_playerEntityID, j))
                         {
                             // Update j transform component in some way using the frequency we got earlier
+                            if (EntityHandler::GetInstance().HasComponents(j, (int)ComponentType::PlatFormPatrolComponent))
+                            {
+                                using namespace DirectX;
+                                PlatformPatrolComponent* t_platformPatrolComp = EntityHandler::GetInstance().GetComponentFromStorage<PlatformPatrolComponent>(j);
+                                RigidBodyComponent* t_rigidBodyComponent = EntityHandler::GetInstance().GetComponentFromStorage<RigidBodyComponent>(j);
+                                TransformComponent* t_transformComponent = EntityHandler::GetInstance().GetComponentFromStorage<TransformComponent>(j);
+                                XMFLOAT3 t_desiredPosition;
+                                // Get the vector between the two points
+                                XMVECTOR t_direction = XMLoadFloat3(&t_platformPatrolComp->startPosition) - XMLoadFloat3(&t_platformPatrolComp->endPosition);
+                                
+                                // Normalize the Frequency to a value between 0 and 1. This is hardcoded atm TODOLH
+                                float t_frequency;
+
+                                //////// STARTA HÄRRRR fixa frekvens från jawwwwarn
+
+                                // Calculate the desiredposition if frequency is held steady: take the vector between start and end. multiply with frequencyvalue 0 to 1. And add the start position
+                                XMStoreFloat3(&t_desiredPosition, t_frequency * t_direction + XMLoadFloat3(&t_platformPatrolComp->startPosition));
+
+                                // Get the distance between desired and actual position of the platform
+                                XMFLOAT3 t_distanceBetweenDesiredAndActualPosition;
+                                XMStoreFloat3(&t_distanceBetweenDesiredAndActualPosition, XMVector3Length(XMLoadFloat3(&t_desiredPosition) - XMLoadFloat3(&t_transformComponent->position)));
+
+                                // Get the normalized Vector between the to maximumpoints Then we move along that vector using the distance between desired and actual position 0.25 is hardcoded TODOLH
+                                XMFLOAT3 t_normalizedDirection;
+                                XMFLOAT3 t_velocityVector;
+                                XMStoreFloat3(&t_velocityVector, XMVector3Normalize(t_direction) * t_distanceBetweenDesiredAndActualPosition.x * 0.25);
+
+                                // Set the velocity of the platform to the vector we just calculated
+                                m_sharedContext.GetPhysicsModule().GetRigidBodyManager().SetBodyVelocity(t_rigidBodyComponent->p_bodyID, t_velocityVector);
+
+
+                            }
                         }
                     }
                 }
