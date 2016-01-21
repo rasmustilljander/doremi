ac12b1561250df2638a2e9dae23a80f4187302cb
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-01-21 13:02:28 +0100

    More userfriendly dynamicloader for errors

--- Utility/DynamicLoader/Source/DynamicLoader.cpp
+++ Utility/DynamicLoader/Source/DynamicLoader.cpp
@@ -8,19 +8,29 @@
 
 namespace DynamicLoader
 {
-    void* LoadSharedLibrary(const char* filename) { return (void*)LoadLibraryA(filename); }
+    void BuildLastError();
+
+    namespace
+    {
+        char* g_lastError = nullptr;
+    }
+
+    void* LoadSharedLibrary(const char* filename)
+    {
+        BuildLastError();
+        return (void*)LoadLibraryA(filename);
+    }
 
     void FreeSharedLibrary(void* library) { FreeLibrary((HMODULE)library); }
 
     void* LoadProcess(void* library, char* name) { return GetProcAddress((HMODULE)library, name); }
 
-    const char* GetLastError()
+    void BuildLastError()
     {
-        char* str = "";
-
+        // delete g_lastError; //TODORT not sure if this is a memory leak or not.
         DWORD error = ::GetLastError();
-        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, 0, error, 0, (LPSTR)&str, 0, 0);
-
-        return str;
+        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, 0, error, 0, (LPSTR)&g_lastError, 0, 0);
     }
+
+    const char* GetLastError() { return g_lastError; }
 }
