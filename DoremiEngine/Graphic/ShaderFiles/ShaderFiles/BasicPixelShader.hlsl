0fa5e12f9540b0b2d03db6b8a649d2216f71100a
Rebecca Künkel <onewiinged@hotmail.com>
2016-01-27 15:42:27 +0100

    FORWARD PLUS WORKING
    I think....

--- DoremiEngine/Graphic/ShaderFiles/ShaderFiles/BasicPixelShader.hlsl
+++ DoremiEngine/Graphic/ShaderFiles/ShaderFiles/BasicPixelShader.hlsl
@@ -55,24 +55,24 @@ Texture2D ObjTexture : register(t0);
 
 SamplerState ObjSamplerState : register(s0);
 
-float3 CalcDirectionalLight(PixelInputType input, int i)
+float3 CalcDirectionalLight(PixelInputType input, Light l)
 {
     float3 lightDir;
     float lightIntensity;
     float4 texcolor = float4(0.0, 0.0, 0.0, 1);
     float3 normal = normalize(input.normal);
 
-    lightDir = -light[i].direction;
+    lightDir = -l.direction;
     lightIntensity = saturate(dot(normal, lightDir));
-    return saturate(light[i].color * lightIntensity) * 0.3;
+    return saturate(l.color * lightIntensity) * 0.3;
 }
 
-float3 CalcSpotLight(PixelInputType input, int i)
+float3 CalcSpotLight(PixelInputType input, Light l)
 {
     return float3(0, 0, 0);
 }
 
-float3 CalcPointLight(PixelInputType input, int i)
+float3 CalcPointLight(PixelInputType input, Light l)
 {
     float4 texcolor = float4(0.5, 0.5, 0.5, 1);
     float3 normal = normalize(input.normal);
@@ -80,23 +80,23 @@ float3 CalcPointLight(PixelInputType input, int i)
     float3 scatteredLight, reflectedLight;
     float attenuation;
 
-    float3 lightDirection = light[i].position - input.worldPos.xyz;
+    float3 lightDirection = l.position - input.worldPos.xyz;
     float lightDistance = length(lightDirection);
 
     if (lightDistance < 500.0f)
     {
         attenuation = 2.5f /
-            (light[i].attenuation[0] + light[i].attenuation[1] * lightDistance +
-                light[i].attenuation[2] * lightDistance * lightDistance);
+            (l.attenuation[0] + l.attenuation[1] * lightDistance +
+                l.attenuation[2] * lightDistance * lightDistance);
 
         float3 halfVector = normalize(lightDirection + normalize(-input.worldPos));
 
         float diffuse = max(0.0, dot(normal, lightDirection));
         float specular = max(0.0, dot(normal, halfVector));
 
-        scatteredLight = light[i].color * diffuse * attenuation;
-        reflectedLight = light[i].color * specular * attenuation;
-        return min(texcolor.rgb * scatteredLight + reflectedLight, float3(1, 1, 1)) * light[i].intensity;
+        scatteredLight = l.color * diffuse * attenuation;
+        reflectedLight = l.color * specular * attenuation;
+        return min(texcolor.rgb * scatteredLight + reflectedLight, float3(1, 1, 1)) * l.intensity;
     }
 
     return float3(0, 0, 0);
@@ -105,6 +105,15 @@ float3 CalcPointLight(PixelInputType input, int i)
 float4 PS_main(PixelInputType input) : SV_TARGET
 {
     float2 screenPos = input.screenPos.xy / input.screenPos.z;
+    screenPos.y = -screenPos.y;
+    screenPos = (screenPos + 1) * 400;
+
+    //calculate which thread group this pixel was in the compute shader stage
+    float2 groupID2 = float2((int)screenPos.x / 16, (int)screenPos.y / 16); 
+    float groupID = groupID2.x + (groupID2.y * 50);
+    //extract data from light grid
+    int index = o_LightGrid[groupID].offset;
+    int value = o_LightGrid[groupID].value;
 
     float4 texcolor = ObjTexture.Sample(ObjSamplerState, input.texCoord);
     texcolor = saturate(texcolor);
@@ -113,23 +122,23 @@ float4 PS_main(PixelInputType input) : SV_TARGET
     
     float3 rgb = float3(0, 0, 0);
 
-    for (int i = 0; i < NUM_LIGHTS; i++)
+    for (int i = index; i < index + value; i++)
     {
-        if (light[i].type == 0)
+        Light l = light[o_LightIndexList[i]];
+        if (l.type == 0)
             rgb += float3(0, 0, 0);
-        if (light[i].type == 1)
-            rgb += CalcDirectionalLight(input, i);
-        if (light[i].type == 2)
-            rgb += CalcSpotLight(input, i);
-        if (light[i].type == 3)
-            rgb += CalcPointLight(input, i);
+        if (l.type == 1)
+            rgb += CalcDirectionalLight(input, l);
+        if (l.type == 2)
+            rgb += CalcSpotLight(input, l);
+        if (l.type == 3)
+            rgb += CalcPointLight(input, l);
 
     }
-    Plane test;
-    test.N = float3(0, 0, 0);
     
-    //return float4(rgb, 1) * texcolor * 3;
-    return float4((t_LightGrid[screenPos.x + (screenPos.y * 800)].value) / 6.f, (t_LightGrid[screenPos.x + (screenPos.y * 800)].value) / 6.f, (t_LightGrid[screenPos.x + (screenPos.y * 800)].value) / 6.f, 1);
+    return float4(rgb, 1) * texcolor * 3;
+    //return float4(screenPos.x/800.f, screenPos.y/800.f, 0, 1);
+    //return float4((t_LightGrid[screenPos.x + (screenPos.y * 800)].value) / 6.f, (t_LightGrid[screenPos.x + (screenPos.y * 800)].value) / 6.f, (t_LightGrid[screenPos.x + (screenPos.y * 800)].value) / 6.f, 1);
     //return float4(4.f/6.f, 4.f/6.f, 4.f/6.f, 1);
 
 }
