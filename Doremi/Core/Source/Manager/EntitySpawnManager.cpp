eb008d104efed9db1652045e6465c6a305d64120
John Bergsten <putte_dt@hotmail.se>
2016-02-02 13:44:53 +0100

    EnemySpawner: Started work on manager
    Have to change name of component... This is getting annoying

--- Doremi/Core/Source/Manager/EntitySpawnManager.cpp
+++ Doremi/Core/Source/Manager/EntitySpawnManager.cpp
@@ -1,6 +1,10 @@
 /// Project specific
 // This class
 #include <Manager/EntitySpawnManager.hpp>
+// Handlers
+#include <EntityComponent/EntityHandler.hpp>
+// Components
+#include <EntityComponent/Components/EntitySpawner.hpp>
 
 namespace Doremi
 {
@@ -14,7 +18,19 @@ namespace Doremi
         EntitySpawnManager::~EntitySpawnManager() {}
 
 
-        void EntitySpawnManager::Update(double p_dt) {}
+        void EntitySpawnManager::Update(double p_dt)
+        {
+            // Loop through all entities
+            const size_t length = EntityHandler::GetInstance().GetLastEntityIndex();
+            for(size_t i = 0; i < length; i++)
+            {
+                // Check that the current entity has the relevant components
+                if(EntityHandler::GetInstance().HasComponents(i, (int)ComponentType::EntitySpawner))
+                {
+                    // We've found an entity spawner
+                }
+            }
+        }
         void EntitySpawnManager::OnEvent(Event* p_event) {}
     }
 }
