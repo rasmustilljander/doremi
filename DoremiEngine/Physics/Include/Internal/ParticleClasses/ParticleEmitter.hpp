f7f9e979d4fe4f479c5a0e87748fcfcdbec4bf64
John Bergsten <putte_dt@hotmail.se>
2016-02-03 16:55:07 +0100

    Made several help methods for update. Also split drain thingy

--- DoremiEngine/Physics/Include/Internal/ParticleClasses/ParticleEmitter.hpp
+++ DoremiEngine/Physics/Include/Internal/ParticleClasses/ParticleEmitter.hpp
@@ -67,15 +67,32 @@ namespace DoremiEngine
             particles (update as more stuff is added)*/
             void Update(float p_dt);
             /**
-                UpdateLifetime of all active particles. Release particles that have lifetime < 0            
-            */
-            void UpdateParticleLifeTimeAndRemoveExpired(float p_dt);
-            /**
-                Sets the lifetime of the particles made after this call. Should be set before every particle call just to be safe that it is the correct timelength
+                Sets the lifetime of the particles made after this call. Should be set before every particle call just to be safe that it is the
+               correct timelength
             */
             void SetParticlesLifeTime(double p_time) { m_lifeTime = p_time; }
 
         private:
+            // Locks read data (do this before using the read data)
+            void LockParticleData();
+            // Unlocks read data (do this after using the read data)
+            void UnlockParticleData();
+
+            /**
+            UpdateLifetime of all active particles. Release particles that have lifetime < 0 */
+            void UpdateParticleLifeTimeAndRemoveExpired(float p_dt);
+            /**
+            Remove particles that have collided with a drain */
+            void RemoveDrainCollidedParticles(float p_dt);
+            /**
+            Updates the emission of particles*/
+            void UpdateParticleEmission(float p_dt);
+
+            /*
+            Data used to store read data about particles
+            Make sure to lock and unlock before/after using this!*/
+            PxParticleReadData* m_readData;
+
             ParticleEmitterData m_this;
             std::map<int, double> m_particlesLifeTime;
             double m_lifeTime;
