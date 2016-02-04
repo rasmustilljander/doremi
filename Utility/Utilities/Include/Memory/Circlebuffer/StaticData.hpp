e2aaff48561c58e7564da3c887a2e7e350d1375b
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-04 18:11:13 +0100

    Fixed losing data bug using Circlebuffer as IPC
    If the circlebuffer on one side was inited long before the other and has a
    chance to actually produce something before the other one was inited. Then
    when the second one was initialized it would overwrite all the metadata
    and thus some data would be lost.
    
    The current solution may not be top notch, as it assumes that a specific
    part of the memory will probably never be a specific value during
    release...
    
    This also reduced the adjusted size of the buffer, and thus some tests
    fails, I had to adjust some values for this. Also added a lot of more
    comments and text on exception for debuggin.

--- Utility/Utilities/Include/Memory/Circlebuffer/StaticData.hpp
+++ Utility/Utilities/Include/Memory/Circlebuffer/StaticData.hpp
@@ -1,10 +1,14 @@
 #pragma once
+#include <cstdint>
 namespace Doremi
 {
     namespace Utilities
     {
         namespace Memory
         {
+            // Hopefully started byte will never be this to begin with...
+            static uint8_t STARTED = 241;
+
             struct StaticData
             {
                 StaticData()
@@ -12,10 +16,12 @@ namespace Doremi
                     currentTail = nullptr;
                     currentHead = nullptr;
                     currentObjectCount = 0;
+                    started = STARTED;
                 }
                 void* currentTail;
                 void* currentHead;
                 size_t currentObjectCount;
+                uint8_t started;
             };
         }
     }
