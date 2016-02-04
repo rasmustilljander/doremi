9949c02301ed71b86c4bdcc3159fa68f6139b8a4
John Bergsten <putte_dt@hotmail.se>
2016-02-04 18:39:20 +0100

    Added manager to sync drain positions. WARNING should crash
    Work in progress, sortof

---
+++ Doremi/Core/Include/Manager/ExtraDrainSyncManager.hpp
@@ -0,0 +1,20 @@
+#pragma once
+// Project specific
+#include <Doremi/Core/Include/Manager/Manager.hpp>
+#include <Doremi/Core/Include/EventHandler/Subscriber.hpp>
+namespace Doremi
+{
+    namespace Core
+    {
+        /**
+        This manager updates the extra drains on some entities*/
+        class ExtraDrainSyncManager : public Manager, public Subscriber
+        {
+        public:
+            ExtraDrainSyncManager(const DoremiEngine::Core::SharedContext& p_sharedContext);
+            virtual ~ExtraDrainSyncManager();
+            void Update(double p_dt) override;
+            void OnEvent(Event* p_event) override;
+        };
+    }
+}
---
+++ Doremi/Core/Source/Manager/ExtraDrainSyncManager.cpp
@@ -0,0 +1,55 @@
+#pragma once
+// This class
+#include <Manager/ExtraDrainSyncManager.hpp>
+
+/// Project specific
+// Engine
+#include <DoremiEngine/Physics/Include/PhysicsModule.hpp>
+#include <DoremiEngine/Physics/Include/RigidBodyManager.hpp>
+
+// Handlers
+#include <EntityComponent/EntityHandler.hpp>
+// Components
+#include <EntityComponent/Components/RigidBodyComponent.hpp>
+#include <EntityComponent/Components/TransformComponent.hpp>
+
+// 3rd party
+#include <DirectXMath.h>
+
+using namespace DirectX;
+namespace Doremi
+{
+    namespace Core
+    {
+        ExtraDrainSyncManager::ExtraDrainSyncManager(const DoremiEngine::Core::SharedContext& p_sharedContext)
+            : Manager(p_sharedContext, "ExtraDrainSyncManager")
+        {
+        }
+
+        ExtraDrainSyncManager::~ExtraDrainSyncManager() {}
+
+        void ExtraDrainSyncManager::Update(double p_dt)
+        {
+            // Update simulation
+            m_sharedContext.GetPhysicsModule().Update(p_dt);
+            const size_t length = EntityHandler::GetInstance().GetLastEntityIndex();
+            int mask = (int)ComponentType::ExtraDrain | (int)ComponentType::Transform;
+            for(size_t i = 0; i < length; i++)
+            {
+                if(EntityHandler::GetInstance().HasComponents(i, mask))
+                {
+                    // Get relevant components
+                    TransformComponent* transComp = EntityHandler::GetInstance().GetComponentFromStorage<TransformComponent>(i);
+
+                    // Get rigid body manager
+                    DoremiEngine::Physics::RigidBodyManager& rigidManager = m_sharedContext.GetPhysicsModule().GetRigidBodyManager();
+
+                    // Set position of drain so it follows us around
+                    rigidManager.SetBodyPosition(i, transComp->position, transComp->rotation);
+                }
+            }
+        }
+
+        void ExtraDrainSyncManager::OnEvent(Event* p_event) {}
+    }
+}
