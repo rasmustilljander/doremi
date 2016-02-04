9d6b40d613aba2d62b46f8ac1e25c19ab14aea5a
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-04 17:26:47 +0100

    Moved much of the code for filemapmutex to cpp

--- Utility/Utilities/Include/IO/FileMap/FileMapMutex.hpp
+++ Utility/Utilities/Include/IO/FileMap/FileMapMutex.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <Windows.h>
 #include <string>
-#include <Utility/Utilities/Include/String/StringHelper.hpp>
 #include <Utility/Utilities/Include/IO/Mutex/Mutex.hpp>
 #include <cstdint>
 
@@ -16,85 +14,35 @@ namespace Doremi
             class FileMapMutex : public Mutex
             {
             public:
-                FileMapMutex() : m_handle(nullptr)
-                {
-                    // TODORT
-                    // TODOXX
-                    // TODOCONFIG
-                    m_lockTime = 10000000000000;
-                }
+                FileMapMutex();
 
-                virtual ~FileMapMutex() { ReleaseMutex(m_handle); }
+                virtual ~FileMapMutex();
 
                 /**
                 Returns true if successful
                 */
-                bool Initialize(const std::string& p_name)
-                {
-                    // Set variables
-                    m_name = p_name;
+                bool Initialize(const std::string& p_name);
 
-                    // Set success variable
-                    bool success = false;
-
-                    // Open/CreateMutex
-                    success = InitializeExternalMutex();
-
-                    if(m_handle)
-                    {
-                        success = true;
-                    }
-                    return success;
-                }
-
-                bool AttemptLock(const uint32_t& p_timeout) override
-                {
-                    DWORD check;
-                    check = WaitForSingleObject(m_handle, p_timeout);
-
-                    if(check == WAIT_ABANDONED)
-                    {
-                        return false; // Did not get mutex
-                    }
-
-                    return true; // Got mutex
-                }
+                /**
+                TODO docs
+                */
+                bool AttemptLock(const uint32_t& p_timeout) override;
 
                 /**
                     Will only return if locked.
                 */
-                void Lock()
-                {
-                    DWORD check;
-                    while(true)
-                    {
-                        // TODORT Define better than 1000?
-                        check = WaitForSingleObject(m_handle, 1000);
-                        if(check == WAIT_OBJECT_0)
-                        {
-                            break;
-                        }
-                    }
-                }
+                void Lock();
 
                 /**
                     TODO docs
                */
-                void Unlock() override { ReleaseMutex(m_handle); }
+                void Unlock() override;
 
             protected:
-                bool InitializeExternalMutex()
-                {
-                    m_handle = CreateMutex(nullptr, FALSE, String::s2ws(m_name).c_str());
-                    if(m_handle != NULL)
-                    {
-                        return true;
-                    }
-                    return false;
-                }
+                bool InitializeExternalMutex();
 
                 std::string m_name;
-                HANDLE m_handle;
+                void* m_handle;
                 size_t m_lockTime;
             };
         }
---
+++ Utility/Utilities/Source/IO/Mutex/FileMapMutex.cpp
@@ -0,0 +1,82 @@
+#include <IO/FileMap/FileMapMutex.hpp>
+
+#include <Windows.h>
+#include <Utility/Utilities/Include/String/StringHelper.hpp>
+
+namespace Doremi
+{
+    namespace Utilities
+    {
+        namespace IO
+        {
+
+            FileMapMutex::FileMapMutex() : m_handle(nullptr)
+            {
+                // TODORT
+                // TODOXX
+                // TODOCONFIG
+                m_lockTime = 10000000000000;
+            }
+
+            FileMapMutex::~FileMapMutex() { ReleaseMutex(m_handle); }
+
+            bool FileMapMutex::Initialize(const std::string& p_name)
+            {
+                // Set variables
+                m_name = p_name;
+
+                // Set success variable
+                bool success = false;
+
+                // Open/CreateMutex
+                success = InitializeExternalMutex();
+
+                if(m_handle)
+                {
+                    success = true;
+                }
+                return success;
+            }
+
+            bool FileMapMutex::AttemptLock(const uint32_t& p_timeout)
+            {
+                DWORD check;
+                check = WaitForSingleObject(m_handle, p_timeout);
+
+                if(check == WAIT_ABANDONED)
+                {
+                    return false; // Did not get mutex
+                }
+
+                return true; // Got mutex
+            }
+
+
+            void FileMapMutex::Lock()
+            {
+                DWORD check;
+                while(true)
+                {
+                    // TODORT Define better than 1000?
+                    check = WaitForSingleObject(m_handle, 1000);
+                    if(check == WAIT_OBJECT_0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            void FileMapMutex::Unlock() { ReleaseMutex(m_handle); }
+
+            bool FileMapMutex::InitializeExternalMutex()
+            {
+                m_handle = CreateMutex(nullptr, FALSE, String::s2ws(m_name).c_str());
+                if(m_handle != NULL)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
