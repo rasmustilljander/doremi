db4d762f5f3a9c00407b67ca928324418a7b1ff3
John Bergsten <putte_dt@hotmail.se>
2016-02-05 11:07:39 +0100

    New rigid body component
    Has lots of fancy stuff so that entityfactory can create rigid bodies

--- Doremi/Core/Include/EntityComponent/Components/RigidBodyComponent.hpp
+++ Doremi/Core/Include/EntityComponent/Components/RigidBodyComponent.hpp
@@ -1,13 +1,37 @@
 #pragma once
+#include <DirectXMath.h>
+using namespace DirectX;
 namespace Doremi
 {
     namespace Core
     {
+        enum class RigidBodyGeometry : int
+        {
+            dynamicSphere,
+            dynamicBox,
+            staticBox,
+            staticSphere,
+            mesh,
+        };
+
+        enum class RigidBodyFlags : int
+        {
+            none,
+            kinematic,
+            trigger,
+            drain,
+            ignoredDEBUG,
+        };
         /**
         Contain the ID for the rigid body of the entity with this component*/
         struct RigidBodyComponent
         {
-            int p_bodyID;
+            // Used for boxes
+            XMFLOAT3 boxDims;
+            // Used for spheres
+            float sphereRadius;
+            RigidBodyGeometry geometry;
+            RigidBodyFlags flags = RigidBodyFlags::none;
         };
     }
 }
