d26906933c2ca85464188afd034c37f683a5e994
John Bergsten <putte_dt@hotmail.se>
2016-02-08 10:42:23 +0100

    Added values to bitmask for rigid body flags
    Can now have multiple

--- Doremi/Core/Include/EntityComponent/Components/RigidBodyComponent.hpp
+++ Doremi/Core/Include/EntityComponent/Components/RigidBodyComponent.hpp
@@ -16,11 +16,11 @@ namespace Doremi
 
         enum class RigidBodyFlags : int
         {
-            none,
-            kinematic,
-            trigger,
-            drain,
-            ignoredDEBUG,
+            none = 0x01,
+            kinematic = 0x02,
+            trigger = 0x04,
+            drain = 0x08,
+            ignoredDEBUG = 0x10,
         };
         /**
         Contain the ID for the rigid body of the entity with this component*/
