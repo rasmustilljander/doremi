2908d6d8c1f2ec3bb2f39d57020d64154103aaf4
Christian <christian_markowicz@hotmail.com>
2016-02-10 10:51:05 +0100

    Added code for HealthChecker class

--- Doremi/Core/Source/HealthChecker.cpp
+++ Doremi/Core/Source/HealthChecker.cpp
@@ -1,4 +1,8 @@
 #include <Doremi/Core/Include/HealthChecker.hpp>
+#include <Doremi/Core/Include/EntityComponent/EntityHandler.hpp>
+#include <Doremi/Core/Include/EntityComponent/Components/HealthComponent.hpp>
+#include <Doremi/Core/Include/EventHandler/Events/RemoveEntityEvent.hpp>
+#include <Doremi/Core/Include/EventHandler/EventHandler.hpp>
 
 namespace Doremi
 {
@@ -21,7 +25,20 @@ namespace Doremi
 
         void HealthChecker::Update()
         {
-            // TODOXX doesn't exist, just template
+            EntityHandler& t_entityHandler = EntityHandler::GetInstance();
+            size_t NumEntities = t_entityHandler.GetLastEntityIndex();
+
+            // Loop over all entities, find health and check if remove
+            for(size_t entityID = 0; entityID < NumEntities; entityID++)
+            {
+                if(t_entityHandler.HasComponents(entityID, (uint32_t)ComponentType::Health))
+                {
+                    if(GetComponent<HealthComponent>(entityID)->currentHealth <= 0)
+                    {
+                        // EventHandler::GetInstance()->BroadcastEvent(new RemoveEntityEvent(entityID));
+                    }
+                }
+            }
         }
     }
 }
