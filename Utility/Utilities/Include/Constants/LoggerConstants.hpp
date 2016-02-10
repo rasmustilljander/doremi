631a2ad177c4903e463406dedbedb53dc9cf7d87
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-10 11:40:54 +0100

    Another variable renamed with _IPC prefix

--- Utility/Utilities/Include/Constants/LoggerConstants.hpp
+++ Utility/Utilities/Include/Constants/LoggerConstants.hpp
@@ -10,8 +10,8 @@ namespace Doremi
             const std::string IPC_DEFAULT_FILEMAP_NAME = std::string("doremi_filemap");
             const size_t IPC_FILEMAP_SIZE = 1024;
             const std::string IPC_FILEMAP_MUTEX_NAME = std::string("doremi_filemap_mutex");
+            const double IPC_FILEMAP_TIMEOUT = 10;
 
-            const double FILEMAP_TIMEOUT = 10;
             const size_t LONGEST_FUNCTION_NAME = 256;
             const size_t LONGEST_MESSAGE_NAME = 256;
             const size_t LONGEST_LINE_NAME = 256;
