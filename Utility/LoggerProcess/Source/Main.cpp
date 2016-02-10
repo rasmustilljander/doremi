3a7e56a2265e0895d9f16b2aaa7c2775ecc6c271
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-10 10:57:10 +0100

    Loggerprocess now takes a UID for creating filemap
    In preparation for having multiple clients and or server on the same
    machine using different loggers.

--- Utility/LoggerProcess/Source/Main.cpp
+++ Utility/LoggerProcess/Source/Main.cpp
@@ -4,15 +4,43 @@
 
 int main(int argc, char** argv)
 {
-    using namespace std;
+    int uniqueIdentifier = 0;
+    if(argc == 1)
+    {
+        // Assume default name
+        using namespace std;
+        cout << "Assumes default name on filemap." << endl;
+    }
+    else if(argc == 2)
+    {
+        using namespace std;
+        // Assume started from game with unique identifier
+        uniqueIdentifier = atoi(argv[1]);
+
+        // Check so that i's not zero
+        if(uniqueIdentifier == 0)
+        {
+            cout << "Invalid unique identifier for filemap." << endl;
+            cout << "Assumes default name on filemap." << endl;
+        }
+    }
+    else
+    {
+        // Something is probably wrong wrong
+        using namespace std;
+        cout << "A confusing amount of arguments: " << argc << "." << endl;
+        cout << "Assumes default name on filemap." << endl;
+    }
+
     LoggerProcess application;
     try
     {
-        application.Initialize();
+        application.Initialize(uniqueIdentifier);
         application.Run();
     }
     catch(std::exception& e)
     {
+        using namespace std;
         cout << "Unknown error: " << e.what() << endl;
 
 #ifdef _DEBUG
@@ -21,8 +49,8 @@ int main(int argc, char** argv)
     }
     catch(...)
     {
+        using namespace std;
         cout << "Unknown error." << endl;
-
         // TODORT debug info
     }
 
