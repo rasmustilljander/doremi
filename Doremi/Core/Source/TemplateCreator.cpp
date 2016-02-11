dbb2c4456c6b64458cffdb781db42c111f3f60fc
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-11 19:36:15 +0100

    Fixed warning float/double

--- Doremi/Core/Source/TemplateCreator.cpp
+++ Doremi/Core/Source/TemplateCreator.cpp
@@ -617,9 +617,9 @@ namespace Doremi
             particleComp->data.m_active = false;
             particleComp->data.m_density = 2.0f;
             particleComp->data.m_dimensions = XMFLOAT2(0.0f, 0.0f);
-            particleComp->data.m_emissionAreaDimensions = XMFLOAT2(3.14 / 4, 3.14 / 5);
-            particleComp->data.m_emissionRate = 0.05;
-            particleComp->data.m_launchPressure = 100;
+            particleComp->data.m_emissionAreaDimensions = XMFLOAT2(3.14f / 4.0f, 3.14f / 5.0f);
+            particleComp->data.m_emissionRate = 0.05f;
+            particleComp->data.m_launchPressure = 100.0f;
             particleComp->data.m_numParticlesX = 5;
             particleComp->data.m_numParticlesY = 1;
             particleComp->data.m_size = 1;
