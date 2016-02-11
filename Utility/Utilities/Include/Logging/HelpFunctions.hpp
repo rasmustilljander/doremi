9d57fb67c00064f83876dd04dc3a62cb31af56d0
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-11 11:31:49 +0100

    Removed unused code

--- Utility/Utilities/Include/Logging/HelpFunctions.hpp
+++ Utility/Utilities/Include/Logging/HelpFunctions.hpp
@@ -24,25 +24,6 @@ namespace Doremi
             TODORT, can move to cpp
             TODORT, docs
             */
-            static std::string BuildLogFileName(const std::string& p_consoleName)
-            {
-                using namespace std;
-                using namespace Doremi::Utilities::String;
-                struct tm now;
-                time_t time_create = time(NULL);
-                localtime_s(&now, &time_create);
-
-                string fileName = string("logs/");
-                fileName.append(intToStringAtleastTwoDigits(now.tm_year + 1900) + "-");
-                fileName.append(intToStringAtleastTwoDigits(now.tm_mon + 1) + "-");
-                fileName.append(intToStringAtleastTwoDigits(now.tm_mday) + "_");
-                fileName.append(intToStringAtleastTwoDigits(now.tm_hour) + ".");
-                fileName.append(intToStringAtleastTwoDigits(now.tm_min));
-                fileName.append("__" + p_consoleName);
-                fileName.append(".txt");
-                return fileName;
-            }
-
             static std::string BuildFolderNameBasedOnTime()
             {
                 using namespace std;
