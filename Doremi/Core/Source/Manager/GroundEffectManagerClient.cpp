a89e6d97bb53c590cd0eb4d46bd0692940886bb1
John Bergsten <putte_dt@hotmail.se>
2016-02-17 14:44:16 +0100

    GroundZoneManagerClient now draws particles (not used yet)
    This is very early version

--- Doremi/Core/Source/Manager/GroundEffectManagerClient.cpp
+++ Doremi/Core/Source/Manager/GroundEffectManagerClient.cpp
@@ -5,12 +5,20 @@
 #include <EntityComponent/EntityHandler.hpp>
 // Components
 #include <EntityComponent\Components\PressureParticleComponent.hpp>
+#include <EntityComponent/Components/TransformComponent.hpp>
 
 /// Engine
 // Physics
 #include <DoremiEngine/Physics/Include/PhysicsModule.hpp>
 #include <DoremiEngine/Physics/Include/FluidManager.hpp>
 #include <DoremiEngine/Physics/Include/RigidBodyManager.hpp>
+// Graphics
+#include <DoremiEngine/Graphic/Include/GraphicModule.hpp>
+#include <DoremiEngine/Graphic/Include/Interface/Manager/MeshManager.hpp>
+#include <DoremiEngine/Graphic/Include/Interface/Manager/DirectXManager.hpp>
+#include <DoremiEngine/Graphic/Include/Interface/Manager/SubModuleManager.hpp>
+#include <DoremiEngine/Graphic/Include/Interface/Mesh/MeshInfo.hpp>
+#include <DoremiEngine/Graphic/Include/Interface/Mesh/MaterialInfo.hpp>
 
 using namespace DirectX;
 namespace Doremi
@@ -29,6 +37,7 @@ namespace Doremi
 
         void GroundEffectManagerClient::Update(double p_dt)
         {
+            // TODOXX This only works if there's ONE particle system
             EntityHandler& entityHandler = EntityHandler::GetInstance();
             const size_t length = entityHandler.GetLastEntityIndex();
             int mask = (int)ComponentType::PressureParticleSystem;
@@ -39,17 +48,20 @@ namespace Doremi
                 {
                     // Merge new positions into already existing positions
                     const vector<XMFLOAT3>& newPositions = m_sharedContext.GetPhysicsModule().GetFluidManager().GetRemovedParticlesPositions(i);
-                    // m_groundEffectPoints.reserve(m_groundEffectPoints.size() + newPositions.size());
-                    // m_groundEffectPoints.insert(m_groundEffectPoints.end(), newPositions.begin(), newPositions.end());
+                    m_groundEffectPoints.reserve(m_groundEffectPoints.size() + newPositions.size());
+                    m_groundEffectPoints.insert(m_groundEffectPoints.end(), newPositions.begin(), newPositions.end());
 
-                    // Loop through new positions
-                    size_t size = newPositions.size();
-                    if(size > 0)
+                    /// Draw the fields
+                    // Get particle component TODOJB Shouldn't draw it as the same mesh and material as the system itself...
+                    ParticlePressureComponent* particleComp = entityHandler.GetComponentFromStorage<ParticlePressureComponent>(i);
+                    // Iterate through all positions
+                    size_t size = m_groundEffectPoints.size();
+                    for(size_t j = 0; j < size; j++)
                     {
-                        for(size_t j = 0; j < size; j++)
-                        {
-                            m_sharedContext.GetPhysicsModule().GetRigidBodyManager().AddShapeToBody(-15, newPositions[j]);
-                        }
+                        XMFLOAT4X4 transMat;
+                        XMStoreFloat4x4(&transMat, XMMatrixTranslation(m_groundEffectPoints[j].x, m_groundEffectPoints[j].y, m_groundEffectPoints[j].z));
+                        m_sharedContext.GetGraphicModule().GetSubModuleManager().GetMeshManager().AddToRenderList(*particleComp->mesh,
+                                                                                                                  *particleComp->material, transMat);
                     }
                 }
             }
