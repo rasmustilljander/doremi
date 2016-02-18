2ebfb3c6cc0371c321833709d5ff33a992a74984
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-18 08:38:53 +0100

    Removed legacy settings in environmentscript

--- prepareCodeEnvironment.sh
+++ prepareCodeEnvironment.sh
@@ -8,10 +8,5 @@ setx.exe SSP_CODE_HOME $PWD
 echo -e "\nCopying git hooks..."
 cp tools/githook/* .git/hooks/
 
-echo -e "\nSetting up .gitconfig"
-gitConfig="git config --global alias.amend \"commit --amend --no-verify\""
-echo $gitConfig
-eval $gitConfig
-
 echo -e "\nPreparing code environment complete"
 read -rsp $'Press any key to exit...\n' -n 1 key
