1c7c3d1d552a0910aecf85c95916c74c4e7e8185
Eric <yooric90@hotmail.com>
2016-02-18 16:27:18 +0100

    OctTree bui8lding boxes
    Seems to work with building boxes!

--- Doremi/Core/Include/TreeCreator.hpp
+++ Doremi/Core/Include/TreeCreator.hpp
@@ -12,21 +12,25 @@ namespace Doremi
         {
             struct OctNode
             {
-                OctNode(uint8_t p_depth, bool p_empty, DirectX::XMFLOAT3 p_boxDimensions)
+                OctNode(uint8_t p_depth, bool p_empty, DirectX::XMFLOAT3 p_boxDimensions, DirectX::XMFLOAT3 p_center)
                 {
                     empty = p_empty;
                     depth = p_depth;
                     boxDimensions = p_boxDimensions;
+                    center = p_center;
                 };
                 OctNode()
                 {
                     empty = true;
                     depth = 0;
                     boxDimensions = DirectX::XMFLOAT3(0, 0, 0);
+                    center = DirectX::XMFLOAT3(0, 0, 0);
                 };
                 ~OctNode(){};
                 DirectX::XMFLOAT3 boxDimensions;
-                std::vector<OctNode> children;
+                DirectX::XMFLOAT3 center;
+                // std::vector<OctNode> children;
+                OctNode* children[8];
                 // Keep them so we can check against them in the kid to this node.
                 std::vector<uint32_t> objectsInTheArea;
                 bool empty;
@@ -43,6 +47,7 @@ namespace Doremi
 
         private:
             void BuildIt(OctNode& o_treeNode);
+            void CreateAndDivideTheChildren(OctNode& o_treeNode);
         };
     }
 }
