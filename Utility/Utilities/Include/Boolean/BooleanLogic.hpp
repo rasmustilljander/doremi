5887ddf7e7ecea0cf134b98a6617737addcf4add
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-02-19 10:42:10 +0100

    Added file infor meeting

---
+++ Utility/Utilities/Include/Boolean/BooleanLogic.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Doremi
+{
+    namespace Utilities
+    {
+        namespace Boolean
+        {
+            /**
+            Inverse XOR
+            */
+            static bool CheckIfBothIsTrueOrBothIsFalse(bool a, bool b) { return true; }
+        }
+    }
+}
