c63e99a22088974ec9609757f7ff0a45d2606be7
Lucas Holmqvist <lucas.holmqvist@gmail.com>
2016-02-22 11:02:24 +0100

    Added event for animation swapping

---
+++ Doremi/Core/Include/EventHandler/Events/AnimationTransitionEvent.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <Doremi/Core/Include/EventHandler/Events/Event.hpp>
+#include <Doremi/Core/Include/EntityComponent/Constants.hpp>
+#include <stdint.h>
+
+
+namespace Doremi
+{
+    namespace Core
+    {
+        enum class Animation
+        {
+            RUN,
+            IDLE,
+            FIRE,
+            HIT,
+            DEATH,
+            JUMP,
+        };
+        struct AnimationTransitionEvent : public Event
+        {
+            AnimationTransitionEvent() : Event(EventType::AnimationTransition) {}
+            AnimationTransitionEvent(uint32_t p_entityID, Animation p_animation)
+                : Event(EventType::AnimationTransition), entityID(p_entityID), animation(p_animation)
+            {
+            }
+
+            /**
+            Write object to stream
+            */
+            void Write(Streamer* p_streamer, uint32_t& op_bitsWritten) override
+            {
+                p_streamer->WriteUnsignedInt32(entityID);
+                p_streamer->WriteUnsignedInt8(static_cast<uint8_t>(animation));
+                op_bitsWritten += sizeof(uint32_t) * 8;
+                op_bitsWritten += sizeof(uint8_t) * 8;
+            }
+
+            /**
+            Read object from stream
+            */
+            void Read(Streamer* p_streamer, uint32_t& op_bitsRead) override
+            {
+                entityID = p_streamer->ReadUnsignedInt32();
+                op_bitsRead += sizeof(uint32_t) * 8;
+                animation = static_cast<Animation>(p_streamer->ReadUnsignedInt8());
+                op_bitsRead += sizeof(uint8_t) * 8;
+            }
+
+            EntityID entityID;
+            Animation animation;
+        };
+    }
+}
