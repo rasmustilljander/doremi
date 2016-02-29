e7e7b99208b99e6ff9e586cb05b3db048851daff
John Bergsten <putte_dt@hotmail.se>
2016-02-29 21:42:55 +0100

    Added new version of sweep method to know hit actor type
    This is only used internally, and so far is only a debug method. Silly to
    do things in engine, but wth

--- DoremiEngine/Physics/Include/Internal/RayCastManagerImpl.hpp
+++ DoremiEngine/Physics/Include/Internal/RayCastManagerImpl.hpp
@@ -13,6 +13,8 @@ namespace DoremiEngine
             int CastRay(const DirectX::XMFLOAT3& p_origin, const DirectX::XMFLOAT3& p_direction, const float& p_range) override;
             int CastRayAgainstCharacterController(const DirectX::XMFLOAT3& p_origin, const DirectX::XMFLOAT3& p_direction, const float& p_range) override;
             int CastSweep(const XMFLOAT3& p_origin, XMFLOAT3& p_direction, float p_width, const float& p_range) override;
+            // DEBUG method. TODOJB remove?
+            int CastSweep(const XMFLOAT3& p_origin, XMFLOAT3& p_direction, float p_width, const float& p_range, int& o_flag);
             std::vector<int> OverlapBoxMultipleHits(const XMFLOAT3& p_origin, const XMFLOAT3& p_halfExtents) override;
 
         private:
--- DoremiEngine/Physics/Source/RayCastManagerImpl.cpp
+++ DoremiEngine/Physics/Source/RayCastManagerImpl.cpp
@@ -101,6 +101,61 @@ namespace DoremiEngine
             return -1;
         }
 
+        int RayCastManagerImpl::CastSweep(const XMFLOAT3& p_origin, XMFLOAT3& p_direction, float p_width, const float& p_range, int& o_flag)
+        {
+            if(p_range <= 0.0f)
+            {
+                cout << "Physcs. Raytracer. Sweep. Range of sweep was zero or below" << endl;
+                return -1;
+            }
+            // Cast directx things to physx
+            PxVec3 origin = PxVec3(p_origin.x, p_origin.y, p_origin.z);
+            PxVec3 direction = PxVec3(p_direction.x, p_direction.y, p_direction.z);
+            direction.normalize();
+            PxSweepBuffer hit; // Used to save the hit
+            /// Paramters for the sweep
+            // PxGeometry* geometry
+            bool status = m_utils.m_worldScene->sweep(PxSphereGeometry(p_width), PxTransform(origin), direction, p_range, hit, PxHitFlag::eMESH_BOTH_SIDES);
+            // hit.block.position;
+            if(!status && !hit.hasBlock)
+            {
+                // No hit detected, return -1 TODOKO Maybee i should return something better?
+                return -1;
+            }
+
+            // Start with checking static and dynamic rigid bodies
+            unordered_map<PxRigidActor*, int> idsByRigidBody = m_utils.m_rigidBodyManager->GetIDsByBodies();
+            if(idsByRigidBody.find(hit.block.actor) != idsByRigidBody.end())
+            {
+                PxRigidActor* actorAsRigic = (PxRigidActor*)hit.block.actor;
+                if(actorAsRigic->isRigidDynamic())
+                {
+                    PxRigidDynamic* actorsAsDynamic = (PxRigidDynamic*)hit.block.actor;
+                    if(actorsAsDynamic->getRigidBodyFlags() & PxRigidBodyFlag::eKINEMATIC)
+                    {
+                        o_flag = 0;
+                    }
+                }
+                return idsByRigidBody.find(hit.block.actor)->second;
+            }
+            else
+            {
+                // Nothing
+            }
+            // Now comes the difficult task of checking vs character controllers
+            unordered_map<PxController*, int> idsByCharacterController = m_utils.m_characterControlManager->GetIdsByControllers();
+            for(auto pairs : idsByCharacterController) // Loop through every pair in the list
+            {
+                if(pairs.first->getActor() == hit.block.actor) // The first part contains the actor pointer
+                {
+                    o_flag = 1;
+                    return pairs.second; // If this is true we found a hit vs character controller, second contains ID
+                }
+            }
+
+            return -1;
+        }
+
         std::vector<int> RayCastManagerImpl::OverlapBoxMultipleHits(const XMFLOAT3& p_origin, const XMFLOAT3& p_halfExtents)
         {
             std::vector<int> returnVec;
