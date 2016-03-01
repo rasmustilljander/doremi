05a1725a6187f4ece1767f44314482b94e63b5bd
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-03-01 14:13:59 +0100

    Readded code for timing handlers on server

--- Doremi/Server/Source/Server.cpp
+++ Doremi/Server/Source/Server.cpp
@@ -238,22 +238,14 @@ namespace Doremi
         // Deliver remove events
         static_cast<Core::EventHandlerServer*>(Core::EventHandler::GetInstance())->DeliverRemoveEvents();
 
-
         Core::PlayerHandler::GetInstance()->Update(p_deltaTime);
 
-        // TODORT
-        // TODOLOG
-        // Utility::Timer::MeasureTimer& timer = Utility::Timer::MeasureTimer::GetInstance();
-        // Have all managers update
         size_t length = m_managers.size();
         for(size_t i = 0; i < length; i++)
         {
-            // TODORT
-            // TODOLOG
-            // Utility::Timer::MeasureInfo& info = timer.GetTimer(m_managers.at(i)->GetName());
-            // info.Reset().Start();
+            Doremi::Core::TimerManager::GetInstance().StartTimer(m_managers.at(i)->GetName());
             m_managers.at(i)->Update(p_deltaTime);
-            // info.Stop();
+            Doremi::Core::TimerManager::GetInstance().StopTimer(m_managers.at(i)->GetName());
         }
 
         TIME_FUNCTION_STOP
