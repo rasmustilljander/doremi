389323b2a57f3ae57854d98e6c21e6799c5659c2
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-03-10 14:51:03 +0100

    Added details to Client.exe when right clicking

---
+++ Doremi/Client/Include/Version.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#define VER_FILEVERSION 1, 0, 0, 0
+#define VER_FILEVERSION_STR "1.1.0.0\0"
+
+#define VER_PRODUCTVERSION 1, 0, 0, 0
+#define VER_PRODUCTVERSION_STR "1.0.0.0\0"
+
+#define VER_COMPANYNAME_STR "Let Him Be Interactive"
+#define VER_FILEDESCRIPTION_STR "The executable which starts the game ArgeeNBeats "
+#define VER_INTERNALNAME_STR "Client.exe"
+#define VER_LEGALCOPYRIGHT_STR "Creative Commons Attribution-NonCommercial 4.0 International Public License"
+#define VER_LEGALTRADEMARKS1_STR "Creative Commons Attribution-NonCommercial 4.0 International Public License"
+#define VER_LEGALTRADEMARKS2_STR VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR "Client.exe"
+#define VER_PRODUCTNAME_STR "ArgeeNBeats"
+#define VER_COMPANYDOMAIN_STR "https://github.com/Meraz/doremi"
